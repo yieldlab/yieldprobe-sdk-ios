2958f451bcd607d96f710f035019937f3e960304
Sven Herzberg <sven.herzberg@cluepunk.com>
2020-06-07 17:23:39 +0200

    🚮 Remove Legacy API

--- Sources/Yieldprobe/PIIDDecoratorFilter.swift
+++ Sources/Yieldprobe/PIIDDecoratorFilter.swift
@@ -20,11 +20,6 @@ struct PIIDDecoratorFilter: URLDecoratorProtocol {
         self.wrapped = wrapped
     }
     
-    init<Wrapped: URLDecoratorProtocol> (configuration: Configuration, wrapped: Wrapped) {
-        self.configuration = configuration
-        self.wrapped = wrapped.decorate(_:)
-    }
-    
     func decorate(_ subject: URL) -> URL {
         guard configuration.personalizeAds else {
             return subject
@@ -37,13 +32,6 @@ struct PIIDDecoratorFilter: URLDecoratorProtocol {
 
 extension URLDecorators {
     
-    static func privacyFilter<Wrapped: URLDecoratorProtocol> (
-        with configuration: Configuration,
-        decorator: Wrapped
-    ) -> URLDecorator {
-        PIIDDecoratorFilter(configuration: configuration, wrapped: decorator).decorate(_:)
-    }
-
     static func privacyFilter (with configuration: Configuration,
                                decorator: @escaping URLDecorator) -> URLDecorator
     {

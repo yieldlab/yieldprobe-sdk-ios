806195a2e21586e7f405e9f43d9400381bb7ef21
Sven Herzberg <sven.herzberg@cluepunk.com>
2020-06-07 17:29:50 +0200

    🚮 Remove Legacy Ad Personalization API

--- Sources/Yieldprobe/URLDecorators+Personalize.swift
+++ Sources/Yieldprobe/URLDecorators+Personalize.swift
@@ -7,12 +7,6 @@
 
 extension URLDecorators {
     
-    static func privacyFilter (with configuration: Configuration,
-                               decorator: @escaping URLDecorator) -> URLDecorator
-    {
-        personalize(if: configuration.personalizeAds, decorator)
-    }
-    
     static func personalize (if condition: Bool, _ decorator: @escaping URLDecorator)
         -> URLDecorator
     {
